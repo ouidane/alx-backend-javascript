7e87e5464cd621f57cc8a01c4105ad350f2ab2ce
ouidane <ouidanezakaria@gmail.com>
2025-04-17 22:14:59 +0100

    task 9

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -34,14 +34,14 @@ ${this._alxClass.year} - ${this._alxClass.location}`;
   }
 }
 
-const class2019 = new ALXClass(2019, "San Francisco");
-const class2020 = new ALXClass(2020, "San Francisco");
+const class2019 = new ALXClass(2019, 'San Francisco');
+const class2020 = new ALXClass(2020, 'San Francisco');
 
-const student1 = new StudentALX("Guillaume", "Salva", class2020);
-const student2 = new StudentALX("John", "Doe", class2020);
-const student3 = new StudentALX("Albert", "Clinton", class2019);
-const student4 = new StudentALX("Donald", "Bush", class2019);
-const student5 = new StudentALX("Jason", "Sandler", class2019);
+const student1 = new StudentALX('Guillaume', 'Salva', class2020);
+const student2 = new StudentALX('John', 'Doe', class2020);
+const student3 = new StudentALX('Albert', 'Clinton', class2019);
+const student4 = new StudentALX('Donald', 'Bush', class2019);
+const student5 = new StudentALX('Jason', 'Sandler', class2019);
 
 const listOfStudents = [student1, student2, student3, student4, student5];
 
