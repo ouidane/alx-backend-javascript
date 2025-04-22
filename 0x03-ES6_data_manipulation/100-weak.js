1b2bc14747fe3ca7721c911c9f73c3d20c2b5a49
ouidane <ouidanezakaria@gmail.com>
2025-04-22 04:54:47 +0100

    task 11

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -2,7 +2,7 @@ export const weakMap = new WeakMap();
 
 export function queryAPI(endpoint) {
   if (!weakMap.has(endpoint)) {
-    weakMap.set(endpoint, 1);
+    weakMap.set(endpoint, 0);
   } else {
     const count = weakMap.get(endpoint);
     if (count >= 5) {
