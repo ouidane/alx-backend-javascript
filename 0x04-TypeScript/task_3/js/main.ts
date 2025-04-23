eaab035d009ba1c4cf94f6f65d583d656b0d20e8
ouidane <ouidanezakaria@gmail.com>
2025-04-23 23:33:12 +0100

    task 3

---
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -0,0 +1,9 @@
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  return `${firstName.charAt(0)}. ${lastName}`;
+};
+
+console.log(printTeacher("John", "Doe"));
