75dfbd9d9f86c926660ef3322e4ba5a4cbc962ca
ouidane <ouidanezakaria@gmail.com>
2025-06-11 00:51:43 +0100

    task 0

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,9 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {string} message - The message to log.
+ * @returns {void}
+ */
+// Arrow function
+const displayMessage = (msg) => console.log(msg);
+
+module.exports = displayMessage;
